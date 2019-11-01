c7a91787fdf0bde62f2ba21cb52838e294bfaec9
akw550 <akhlaqwarraich550@gmail.com>
2019-11-01 12:52:45 +0500

    first commit

---
+++ pro1/pro1/mysite/mysite/views.py
@@ -0,0 +1,40 @@
+# i have created this file -AK
+
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+# def index(request):
+#     return HttpResponse("Hello")
+
+# def about(request):
+#     return HttpResponse("About Page")
+
+
+#           -------------------
+
+def index(request):
+    # return HttpResponse("Home")
+    # param = {'name' : 'Akhlaq', 'place' : 'Earth'}
+
+    return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def analyze(request):
+    #Get the text
+    djtext = request.GET.get('text', 'default')
+    print(djtext)
+    #Analyze the text
+    return HttpResponse("remove punc")
+
+
+# def capfirst(request):
+#     return HttpResponse("capitalize first <a href=http://127.0.0.1:8000>Back</a>")
+
+# def newlineremove(request):
+#     return HttpResponse("new line remove <a href=http://127.0.0.1:8000>Back</a>")
+
+# def spaceremove(request):
+    return HttpResponse("space remove <a href=http://127.0.0.1:8000>Back</a>")
